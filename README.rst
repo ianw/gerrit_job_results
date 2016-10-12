a068409442d0b0bcb2e548d9bd531ea076cd30c5
Ian Wienand <iwienand@redhat.com>
2016-10-12 18:05:57 +1100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,5 @@
+==================
+gerrit-job-results
+==================
+
+A dashboard for helping debug various jobs in openstack gerrit
---
+++ gerrit_job_results/gerrit_job_results.py
@@ -0,0 +1,125 @@
+import codecs
+import datetime
+import logging
+import os.path
+
+import gerritlib.gerrit
+
+from jinja2 import Environment
+from jinja2 import PackageLoader
+
+logger = logging.getLogger(__name__)
+
+GERRIT_HOST = 'review.openstack.org'
+USER = 'iwienand'
+KEY = os.path.expanduser('~/.ssh/id_rsa')
+
+
+class Change(object):
+
+    def __init__(self,
+                 project, branch, number, timestamp, subject):
+        self.project = project
+        self.branch = branch
+        self.number = number
+        self.timestamp = timestamp
+        self.subject = subject
+        self.tests = []
+
+    def __str__(self):
+        s = "%s %s %s %s\n" % (self.timestamp,
+                               self.number, self.branch, self.subject)
+        for t in self.tests:
+            s += " %s\n" % t
+        return s
+
+
+class TestRun(object):
+
+    def __init__(self,
+                 name, logs, status, runtime):
+        self.name = name
+        self.logs = logs
+        self.status = status
+        self.runtime = runtime
+
+    def __str__(self):
+        return "%s : %s (%s)" % (self.name, self.status, self.runtime)
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    logger.info("starting")
+
+    all_changes = []
+
+    g = gerritlib.gerrit.Gerrit(GERRIT_HOST, USER, keyfile=KEY)
+    results = g.bulk_query(
+        '--comments status:open project:openstack-dev/devstack limit:100')
+
+    for change in results:
+        latest = None
+
+        try:
+            logging.info("Considering %s" % change['number'])
+            # find the latest comment by jenkins
+            for comment in change['comments']:
+                if comment['reviewer']['username'] == 'jenkins':
+                    latest = comment
+        except Exception:  # can happen for last summary comment
+            pass
+
+        if not latest:
+            logging.info("No jenkins comments, skipping")
+            continue
+
+        timestamp = datetime.datetime.fromtimestamp(latest['timestamp'])
+
+        change = Change('devstack', change['branch'],
+                        change['number'], timestamp, change['subject'])
+
+        results = latest['message'].split('\n')
+        for result in results:
+            if result.startswith("-"):
+                jobs = result[2:].split(' ')
+                # figure out runtime
+                j = TestRun(jobs[0], jobs[1], jobs[3], "XXX")
+                change.tests.append(j)
+
+        all_changes.append(change)
+
+        fedora_24_changes = []
+        for change in all_changes:
+            for run in change.tests:
+                if run.name == "gate-tempest-dsvm-platform-fedora24-nv":
+                    fedora_24_changes.append(dict(timestamp=change.timestamp,
+                                                  number=change.number,
+                                                  subject=change.subject,
+                                                  branch=change.branch,
+                                                  run=run))
+        fedora_24_changes.sort(key=lambda x: x['timestamp'], reverse=True)
+
+        centos_changes = []
+        for change in all_changes:
+            for run in change.tests:
+                if run.name == "gate-tempest-dsvm-platform-centos7-nv":
+                    centos_changes.append(dict(timestamp=change.timestamp,
+                                               number=change.number,
+                                               subject=change.subject,
+                                               branch=change.branch,
+                                               run=run))
+        centos_changes.sort(key=lambda x: x['timestamp'], reverse=True)
+
+        env = Environment(
+            loader=PackageLoader('gerrit_job_results', 'templates'))
+        template = env.get_template('page.html')
+
+        output = template.render(all_changes=all_changes,
+                                 fedora_24_changes=fedora_24_changes,
+                                 centos_changes=centos_changes)
+
+        with codecs.open('output.html', 'w', 'utf-8') as f:
+            f.write(output)
+
+if __name__ == "__main__":
+    main()
